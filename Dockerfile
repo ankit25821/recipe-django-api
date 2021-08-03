a48366a0018ac144bcca8a171f354b46ff40280d
amit tiwari <amitgit712@gmail.com>
2021-08-03 16:50:29 +0530

    added test commands file

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,11 @@ LABEL authors='Ankit'
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt ./requirements.txt
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .temp-build-deps \
+    gcc libc-dev linux-headers postgresql-dev
 RUN pip install -r /requirements.txt
+RUN apk del .temp-build-deps
 
 RUN mkdir /app
 WORKDIR /app
--- app/core/admin.py
+++ app/core/admin.py
@@ -6,16 +6,17 @@ from core import models
 
 
 class UserAdmin(BaseUserAdmin):
+
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-        (None, {'fields':('email', 'password')}),
-        (_('Personal Info'),{'fields':('name',)}),
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
-            {'fields':('is_active', 'is_staff', 'is_superuser')}
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
         ),
-        (_('Important dates'), {'fields':('last_login',)})
+        (_('Important dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
         (None, {
