cbaf87e862b4d07f288cd5cdc5529b562e93c05d
amit tiwari <amitgit712@gmail.com>
2021-08-03 15:31:25 +0530

    added admin page test

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
-from core.models import User
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-admin.site.register(User)
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields':('email', 'password')}),
+        (_('Personal Info'),{'fields':('name',)}),
+        (
+            _('Permissions'),
+            {'fields':('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates'), {'fields':('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
