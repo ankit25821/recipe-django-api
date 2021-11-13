cc2f9e74b5b937afca48cfb41a880b53317ae7d5
ankit <ankitgit21@gmail.com>
2021-11-13 20:23:45 +0530

    added image field to recipe model.

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,10 +127,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+
+STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
